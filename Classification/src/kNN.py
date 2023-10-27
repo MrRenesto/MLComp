ef70af097a027e8cc82275a108a5e6301b79104a
René <r-sauermann@web.de>
2023-10-27 10:17:15 +0200

    Knn Loop through k variable

--- Classification/src/kNN.py
+++ Classification/src/kNN.py
@@ -20,6 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=
 
 
 X_test = X_test.drop('Id', axis=1)
+X_train = X_train.drop('Id', axis=1)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -28,46 +29,50 @@ X_test = scaler.transform(X_test)
 # Step 2: Choose a Machine Learning Algorithm
 from sklearn.neighbors  import KNeighborsClassifier
 
-# Choose a classification algorithm
-clf = KNeighborsClassifier(n_neighbors=5)
 
-# Step 3: Train the Model
-clf.fit(X_train, y_train)
+# Create an empty list to store accuracy scores
+accuracy_scores_test = []
+accuracy_scores_train = []
 
-# Step 4: Evaluate the Model
-
-print(X_test)
-# Make predictions on the test set
-y_pred = clf.predict(X_test)
-
+# Define a range of values for n_neighbors to try
+n_neighbors_values = range(1, 101)
 y_test_array = y_test['label'].to_numpy()
+y_train_array = y_train['label'].to_numpy()
 
-# Now, y_test_array contains the labels
-print(y_test_array)
+for n in n_neighbors_values:
+    clf = KNeighborsClassifier(n_neighbors=n)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    y_pred_train = clf.predict(X_train)
+    accuracy = accuracy_score(y_test_array, y_pred[:, 1])
+    accuracy_scores_test.append(accuracy)
 
-# Extract predicted labels as a NumPy array
-y_pred_labels = y_pred[:, 1]
-
-# Now, y_pred_array contains the predicted labels
-print(y_pred_labels)
-
-# Calculate accuracy for binary classification
-accuracy = accuracy_score(y_test_array, y_pred_labels)
-
-print(f"Accuracy: {accuracy:.2f}")
-
-# Print a classification report with more metrics
-print(classification_report(y_test_array, y_pred_labels))
+    accuracy2 = accuracy_score(y_train_array, y_pred_train[:, 1])
+    accuracy_scores_train.append(accuracy2)
 
 
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_neighbors_values, accuracy_scores_test, marker='.')
+plt.plot(n_neighbors_values, accuracy_scores_train, marker='.')
+plt.title('n_neighbors vs. Accuracy')
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.grid(True)
+plt.show()
+
+
+"""
 validation_data = pd.read_csv('..\\Data\\test_features.csv')
 
 # 2. Extract the features from the validation data
 X_val = validation_data  # All columns except the 'Id' column
+X_val = X_val.drop('Id', axis=1)
 
 # 3. Make predictions using your trained model
 # Replace 'your_model' with the actual variable that holds your trained model
-y_pred_val = clf.predict(X_val)
+y_pred_val = clf.predict(X_val.values)
 y_pred_val = [pred[1] for pred in y_pred_val]
 
 # 4. Create a DataFrame with the predictions and the 'Id' column
@@ -79,3 +84,5 @@ file_name = f'predictions_validation_{current_datetime}.csv'
 
 # Save the DataFrame to the CSV file
 predictions_df.to_csv(file_name, index=False)
+
+"""
